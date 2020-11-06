e0f83f442a20e1d3d95dded38b76e04bf4267531
Jerome Bollinger <jerome.f.bollinger@gmail.com>
2020-11-06 10:32:13 +0100

    #just a git staging Test

--- src/main/java/ch/trinat/example/hellofx/HelloFX.java
+++ src/main/java/ch/trinat/example/hellofx/HelloFX.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
+// I added nothing
 public class HelloFX extends Application {
 
     @Override
