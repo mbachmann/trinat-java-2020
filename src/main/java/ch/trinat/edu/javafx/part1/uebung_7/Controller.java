9c9dd95262e1e29b6c335e3fd15eb09a344b4ebf
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-11-09 17:08:18 +0100

    added exercice 7 classes, but it does not work

---
+++ src/main/java/ch/trinat/edu/javafx/part1/uebung_7/Controller.java
@@ -0,0 +1,25 @@
+package ch.trinat.edu.javafx.part1.uebung_7;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+	@FXML
+	 private PasswordField passwordField;
+	 @FXML
+	 private TextField nameField;
+	@FXML
+	 private Button btn;
+	 @FXML
+
+	 protected void print(ActionEvent event)
+	 { System.out.println("Signin button user input: "
+				 + nameField.getText() + " pwd: "
+				 + passwordField.getText());
+	 btn.setText("cahnge");
+
+	}}
+
---
+++ src/main/java/ch/trinat/edu/javafx/part1/uebung_7/Launcher.java
@@ -0,0 +1,5 @@
+package ch.trinat.edu.javafx.part1.uebung_7;
+
+public class Launcher {
+
+}
