3486718a6777d97eb5498efc83ba47826e904d34
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-11-09 17:02:13 +0100

    added exercice 6

---
+++ src/main/java/ch/trinat/edu/javafx/part1/uebung_6/Launcher.java
@@ -0,0 +1,10 @@
+package ch.trinat.edu.javafx.part1.uebung_6;
+
+import ch.trinat.example.hellofx.HelloFX;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+    	uebung_6.main(args);
+    }
+}
---
+++ src/main/java/ch/trinat/edu/javafx/part1/uebung_6/uebung_6.java
@@ -0,0 +1,79 @@
+package ch.trinat.edu.javafx.part1.uebung_6;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class uebung_6 extends Application{
+	
+	public void start(Stage primaryStage) {
+		try {
+		// Create MenuBar
+		MenuBar menuBar = new MenuBar();
+		
+		// Create menus
+		Menu fileMenu = new Menu("File");
+		Menu editMenu = new Menu("Edit");
+		Menu helpMenu = new Menu("Help");
+		
+		// Create MenuItems
+		MenuItem newItem = new MenuItem("New");
+		//newItem.setOnAction(this);
+		MenuItem openFileItem = new MenuItem("Open File");
+		MenuItem exitItem = new MenuItem("Exit");
+		
+		// Set Accelerator for Exit MenuItem.
+		exitItem.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
+		
+		// When user click on the Exit item, use anonymous inner class
+		exitItem.setOnAction(new EventHandler<ActionEvent>() {
+		 @Override
+		 public void handle(ActionEvent event) {
+		 System.exit(0);
+		 }
+		});
+		MenuItem copyItem = new MenuItem("Copy");
+		MenuItem pasteItem = new MenuItem("Paste");
+		
+		// Add menuItems to the Menus
+		fileMenu.getItems().addAll(newItem, openFileItem, exitItem);
+		editMenu.getItems().addAll(copyItem, pasteItem);
+		
+		// RadioMenuItem
+		RadioMenuItem updateItem1 = new RadioMenuItem("Auto Update");
+		RadioMenuItem updateItem2 = new RadioMenuItem("Ask for Update");
+		ToggleGroup group = new ToggleGroup();
+		updateItem1.setToggleGroup(group);
+		updateItem2.setToggleGroup(group);
+		updateItem1.setSelected(true);
+		helpMenu.getItems().addAll(updateItem1,updateItem2);
+		
+		// Add Menus to the MenuBar
+		menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu);
+		BorderPane root = new BorderPane();
+		root.setTop(menuBar);
+		Scene scene = new Scene(root, 350, 200);
+		primaryStage.setTitle("JavaFX Menu");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		} catch(Exception e) {
+		e.printStackTrace();
+		}
+		}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
