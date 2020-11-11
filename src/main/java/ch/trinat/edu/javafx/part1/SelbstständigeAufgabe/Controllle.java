de710528db74d5acc18dc9d8e6fb79b15b114f7f
MatisGourdes <73692139+MatisGourdes@users.noreply.github.com>
2020-11-11 13:42:22 +0100

    Selbständige übung

---
+++ src/main/java/ch/trinat/edu/javafx/part1/SelbstständigeAufgabe/Controllle.java
@@ -0,0 +1,36 @@
+package ch.trinat.edu.javafx.part1.SelbstständigeAufgabe;
+import java.util.Vector;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class Controllle {
+	Vector<String> antworten = new Vector<String>();
+ 
+@FXML
+ private Button klick;
+ @FXML
+  
+ 
+ public Vector<String> antworten () {
+	
+	antworten.add("Kino");
+	antworten.add("Fernsehen");
+	antworten.add("Nichts");
+	antworten.add("Essen gehen");
+	antworten.add("Schlaffen");
+	antworten.add("Party");
+	 return antworten;}
+ 
+ public void change (ActionEvent event)
+	{ antworten();
+		random();
+	}
+ public void random () {
+			int random=(int) (Math.random() * 6);
+		 klick.setText(antworten.get(random));
+		 }
+		}
+
---
+++ src/main/java/ch/trinat/edu/javafx/part1/SelbstständigeAufgabe/Launcher.java
@@ -0,0 +1,10 @@
+package ch.trinat.edu.javafx.part1.SelbstständigeAufgabe;
+
+
+
+public class Launcher {
+	
+    public static void main (String[] args) {
+    EntscheidungsKnopf.main(args);
+}
+}
