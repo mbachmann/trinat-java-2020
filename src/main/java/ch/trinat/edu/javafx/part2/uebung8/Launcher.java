178926c581568b97ac405bbb5bbaab467d8cb867
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-11-13 08:50:28 +0100

    added exercice 8

---
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung8/Launcher.java
@@ -0,0 +1,8 @@
+package ch.trinat.edu.javafx.part2.uebung8;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+    	uebung8.main(args);
+    }
+}
---
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung8/uebung8.java
@@ -0,0 +1,32 @@
+package ch.trinat.edu.javafx.part2.uebung8;
+
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class uebung8 extends Application {
+		@Override
+		public void start(Stage stage) {
+		
+		TextField text = new TextField ();
+		Slider slider = new Slider(1, 11, 5);
+		slider.setShowTickMarks(true);
+		slider.setShowTickLabels(true);
+		slider.setMajorTickUnit(1);
+		slider.setBlockIncrement(1);
+		text.textProperty().bind(Bindings.format("%.2f", slider.valueProperty()));
+		VBox layout = new VBox(10, text, slider);
+		layout.setStyle("-fx-padding: 10px; -fx-alignment: baseline-right");
+		stage.setScene(new Scene(layout, 300, 100));
+		stage.setTitle("Goes to");
+		stage.show();
+		}
+		public static void main(String[] args) {
+		launch(args);
+		}
+		}
+
