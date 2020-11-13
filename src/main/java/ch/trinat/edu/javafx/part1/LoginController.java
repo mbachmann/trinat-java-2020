54686adbcd9ac9af771e288dd0560b34f460ddef
Mischa Kissling <mischa@kissling.ch>
2020-11-13 12:50:06 +0100

    edit Login

---
+++ src/main/java/ch/trinat/edu/javafx/part1/LoginController.java
@@ -0,0 +1,20 @@
+package ch.trinat.edu.javafx.part1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	@FXML
+	private PasswordField passwordField;
+	
+	@FXML
+	private TextField nameField;
+	
+	@FXML
+	protected void handleSubmitButtonAction(ActionEvent event) {
+		System.out.println("Signin button user input: " + nameField.getText() + " pwd: " + passwordField.getText());
+	}
+	
+}
