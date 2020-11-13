0b5cec7545c8b1c2fd495b06d0a2a046d1b69b35
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-11-13 08:57:35 +0100

    added exercices 13 and 14

---
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung13/Launcher.java
@@ -0,0 +1,9 @@
+package ch.trinat.edu.javafx.part2.uebung13;
+
+
+public class Launcher {
+
+    public static void main(String[] args) {
+    	uebung13.main(args);
+    }
+}
---
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung14/Launcher.java
@@ -0,0 +1,9 @@
+package ch.trinat.edu.javafx.part2.uebung14;
+
+
+public class Launcher {
+
+    public static void main(String[] args) {
+    	uebung14.main(args);
+    }
+}
