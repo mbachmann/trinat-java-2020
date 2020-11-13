697a628a1d6daac9c1b61a86388158daff776681
Enzo JOB <enzojob@orange.fr>
2020-11-13 09:48:08 +0100

    added JavaSliderField.java

--- src/main/java/ch/trinat/edu/javafx/part2/uebung09/JavaSliderField.java
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung09/JavaSliderField.java
@@ -1,5 +1,7 @@
 package ch.trinat.edu.javafx.part2.uebung09;
 
+import java.text.NumberFormat;
+
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.scene.Scene;
@@ -12,12 +14,17 @@ public class JavaSliderField extends Application {
 	@Override
 	public void start(Stage stage) {
 		TextField textField = new TextField ();
-		Slider slider = new Slider(1, 11, 5);
+		Slider slider = new Slider(1, 10, 5);
 		slider.setShowTickMarks(true);
 		slider.setShowTickLabels(true);
 		slider.setMajorTickUnit(1);
 		slider.setBlockIncrement(1);
-		textField.textProperty().bind(Bindings.format("%.2f", slider.valueProperty()));
+		
+	    final double INIT_VALUE = 5;
+        slider.setValue(INIT_VALUE);
+        textField.setText(new Double(INIT_VALUE).toString());
+        textField.textProperty().bindBidirectional(slider.valueProperty(), NumberFormat.getNumberInstance());
+        
 		VBox layout = new VBox(10, textField, slider);
 		layout.setStyle("-fx-padding: 10px; -fx-alignment: baseline-right");
 		stage.setScene(new Scene(layout, 300, 100));
