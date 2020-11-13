1d0ff51c809db40d59889dff577e5c4e401a4324
bacn <mbachmann@trdi.io>
2020-11-13 11:10:58 +0100

    log4j added in pom sample in unit test 5

--- src/test/java/ch/trinat/example/junit/junit5/x/EuroTest.java
+++ src/test/java/ch/trinat/example/junit/junit5/x/EuroTest.java
@@ -1,5 +1,7 @@
 package ch.trinat.example.junit.junit5.x;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EuroTest {
+	private static final Logger LOG = LogManager.getLogger(EuroTest.class);
 	private Euro two;
 
 	@BeforeEach
@@ -18,6 +21,7 @@ public class EuroTest {
 	public void testAdding() throws Exception {
 		two.add(two);
 		assertEquals(new Euro(4.0).getAmount(), two.getAmount(), 0.01);
+		LOG.debug("Unit Test of testAdding successfull {}", two.getAmount());
 	}
 
 	@Test
