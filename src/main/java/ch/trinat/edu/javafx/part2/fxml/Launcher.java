b3512b620485016fe0908da65f164a030ed8d38a
Theo Rey <reytheopro@gmail.com>
2020-11-13 11:59:38 +0100

    changes

---
+++ src/main/java/ch/trinat/edu/javafx/part2/fxml/Launcher.java
@@ -0,0 +1,9 @@
+package ch.trinat.edu.javafx.part2.fxml;
+
+import ch.trinat.example.javafx.Main;
+
+public class Launcher {
+	 public static void main (String[] args) {
+	 Main.main(args);
+	 }
+	}
