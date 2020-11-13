5e8de6b2f66a70cc690e238a37d1bee25ff1158b
Enzo JOB <enzojob@orange.fr>
2020-11-13 10:10:02 +0100

    added rest of javaFX Part2

---
+++ src/main/java/ch/trinat/edu/javafx/part2/uebung15/ProgressThreadExample.java
@@ -0,0 +1,51 @@
+package ch.trinat.edu.javafx.part2.uebung15;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ProgressThreadExample extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) {
+		System.out.println("start: thread = " + Thread.currentThread().getName());
+		primaryStage.setTitle("JavaFX App");
+		ProgressBar bar = new ProgressBar(0);
+		final Label info = new Label();
+		VBox vBox = new VBox(bar, info);
+		Scene scene = new Scene(vBox, 960, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		Thread taskThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				double progress = 0;
+				for (int i = 0; i < 10; i++) {
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					progress += 0.1;
+					final double reportedProgress = progress;
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							bar.setProgress(reportedProgress);
+							info.setText("Progress: " +
+									String.format("%.1g%n",reportedProgress));
+						}
+					});
+				}
+			}
+		});
+		taskThread.start();
+	}
+}
