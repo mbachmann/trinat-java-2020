1d64854a9c3711eed03f66ce6b1758a1b61f0981
BollingerJerome <73817608+BollingerJerome@users.noreply.github.com>
2020-11-13 09:52:44 +0100

    Slider with binding added

---
+++ src/main/java/ch/trinat/edu/javafx/part2/BinderSlider.java
@@ -0,0 +1,34 @@
+package ch.trinat.edu.javafx.part2;
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+
+public class BinderSlider extends Application{
+
+	@Override
+	public void start(Stage stage) {
+		TextField textField = new TextField();
+		Slider slider = new Slider(1, 11, 5);
+		slider.setShowTickMarks(true);
+		slider.setShowTickLabels(true);
+		slider.setMajorTickUnit(1);
+		slider.setBlockIncrement(1);
+		textField.textProperty().bindBidirectional(slider.valueProperty(), new NumberStringConverter());
+		VBox layout = new VBox(10, textField, slider);
+		layout.setStyle("-fx-padding: 10px; -fx-alignment: baseline-right");
+		stage.setScene(new Scene(layout, 300, 100));
+		stage.setTitle("Goes to");
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/main/java/ch/trinat/edu/javafx/part2/Launcher.java
@@ -0,0 +1,9 @@
+package ch.trinat.edu.javafx.part2;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		BinderSlider.main(args);
+	}
+
+}
