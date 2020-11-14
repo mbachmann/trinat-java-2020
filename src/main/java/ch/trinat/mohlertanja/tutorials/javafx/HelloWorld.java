52cddd05b9fa4f6cd176ec2c5fc29fdc3daa507a
Tanja Mohler <tanja.mohler@protonmail.ch>
2020-11-14 20:51:23 +0100

    Übung 2

--- src/main/java/ch/trinat/mohlertanja/tutorials/javafx/HelloWorld.java
+++ src/main/java/ch/trinat/mohlertanja/tutorials/javafx/HelloWorld.java
@@ -1,8 +1,14 @@
 package ch.trinat.mohlertanja.tutorials.javafx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -20,10 +26,30 @@ public class HelloWorld extends Application {
 		root.getChildren().add(label1);
 		root.getChildren().add(label2);
 		
+		root.getChildren().add(createButton());
+		
 		Scene scene = new Scene(root, 600, 300, Color.AZURE);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Java FX Basic Application");
 		primaryStage.show();
+	}
+	
+	Pane createButton() {
+		final Button button = new Button();
+		button.setText("Add a 'Hello World' Label");
+		final Pane pane = new FlowPane();
+		pane.setPadding(new Insets(7, 7, 7, 7));
+		pane.getChildren().add(button);
+		
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				pane.getChildren().add(new Label("- Hello World! -"));
+			}
+		});
+		
+		
+		return pane;
 	}
 	
 	public static void main(String args[]) {
