a410d5316e9eb946f32e2a50f7a9f02f4208b079
Tanja Mohler <tanja.mohler@protonmail.ch>
2020-11-15 19:19:00 +0100

    Selbstständige Aufgabe 1

---
+++ src/main/java/ch/trinat/mohlertanja/abgabe/javafx1/EntscheidungsKnopf.java
@@ -0,0 +1,64 @@
+package ch.trinat.mohlertanja.abgabe.javafx1;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class EntscheidungsKnopf extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			
+			VBox root = new VBox();
+			Scene scene = new Scene(root, 600, 300, Color.AZURE);
+			root.setPadding(new Insets(10));
+			
+			root.getChildren().add(createButton());
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Was machen wir heute Abend?");
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	Pane createButton() {
+		final Button button = new Button();
+		button.setText("Klick mich");
+		final Pane pane = new FlowPane();
+		pane.setPadding(new Insets(7, 7, 7, 7));
+		pane.getChildren().add(button);
+		
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				button.setText(randomAnswer());
+			}
+		});
+				
+		return pane;
+	}
+	
+public String randomAnswer() {
+		String[] todaysPlan = {	"Wir gehen ins Kino", "Wir gehen an ein Konzert", "Wir gehen essen", "Wir gehen spazieren", "Wir sehen uns einen Film an", "Wir gamen", "Wir gehen tanzen"};
+		int random = (int) (Math.random()*7);
+		return todaysPlan[random];
+	}
+	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/ch/trinat/mohlertanja/abgabe/javafx1/Launcher.java
@@ -0,0 +1,9 @@
+package ch.trinat.mohlertanja.abgabe.javafx1;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		EntscheidungsKnopf.main(args);
+	}
+
+}
