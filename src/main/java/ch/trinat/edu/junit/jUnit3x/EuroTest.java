e237b47523794357bee28b352985dcfbc1fc1374
Mischa Kissling <mischa@kissling.ch>
2020-11-20 10:17:21 +0100

    EuroTest JUnit3x

--- src/main/java/ch/trinat/edu/junit/jUnit3x/EuroTest.java
+++ src/main/java/ch/trinat/edu/junit/jUnit3x/EuroTest.java
@@ -3,10 +3,11 @@ package ch.trinat.edu.junit.jUnit3x;
 import junit.framework.TestCase;
 
 public class EuroTest extends TestCase{
-
 	private Euro two;
+	
 	protected void setUp() throws Exception {
 	}
+	
 	public void testAdding() throws Exception {
 		two.add(two);
 		assertEquals(new Euro(4.0).getAmount(), two.getAmount());
