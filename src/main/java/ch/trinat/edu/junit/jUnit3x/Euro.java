6ab74518cb6a6258ab9879b95348000d9c10a656
Mischa Kissling <mischa@kissling.ch>
2020-11-20 10:09:43 +0100

    JUnit Übung 1

---
+++ src/main/java/ch/trinat/edu/junit/jUnit3x/Euro.java
@@ -0,0 +1,19 @@
+package ch.trinat.edu.junit.jUnit3x;
+
+public class Euro {
+
+	private double amount;
+	
+	public Euro(double amount) {
+		this.amount = amount;
+	}
+	
+	public double getAmount() {
+		return this.amount;
+	}
+	
+	public void add (Euro euro) {
+		if (euro.getAmount() <0) throw new IllegalArgumentException();
+		this.amount += euro.getAmount();
+	}
+}
---
+++ src/main/java/ch/trinat/edu/junit/jUnit3x/EuroTest.java
@@ -0,0 +1,13 @@
+package ch.trinat.edu.junit.jUnit3x;
+
+import junit.framework.TestCase;
+
+public class EuroTest extends TestCase{
+	
+	private Euro two;
+	protected void setUp() throws Exception {
+	}
+	public void testAdding() throws Exception {
+	}
+	
+}
