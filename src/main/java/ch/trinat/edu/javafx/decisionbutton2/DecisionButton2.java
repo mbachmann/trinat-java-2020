6ffe910ec4f37a11895debbeb9840189c694bab5
Mischa Kissling <mischa@kissling.ch>
2020-11-20 08:31:23 +0100

    not fixed problems

--- src/main/java/ch/trinat/edu/javafx/decisionbutton2/DecisionButton2.java
+++ src/main/java/ch/trinat/edu/javafx/decisionbutton2/DecisionButton2.java
@@ -1,7 +1,8 @@
 package ch.trinat.edu.javafx.decisionbutton2;
 
+import java.util.Vector;
+
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
 import javafx.collections.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -9,40 +10,46 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class DecisionButton2 extends Application {
-	
-public String zufaelligeAntwort(String[] options) {
-		
-		String[] antwort = options;
-		
-		int random = (int)(Math.random()*antwort.length);
-		return antwort[random];
+
+	public String zufaelligeAntwort(Vector <String> options) {
+
+		int random = (int)(Math.random()*options.size());
+		return options.get(random);
 	}
 
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		primaryStage.setTitle("Entscheidungsknopf");
+		Vector <String> auswahl = null;
+		String[] events = {"Kino", "Konzert", "Essen gehen", "Spazieren gehen", "Fernsehen", "Rheinschwimmen", "Bleiben Sie Zuhause (Alain Berset, 2020)"};
+
+		for (int i = 0; i < events.length; i++) {
+			auswahl.add(events[i]);
+		}
+
+		ObservableList<String> entries = FXCollections.observableArrayList (auswahl);
+		ListView<String> listView = new ListView<>(entries);
 		
-		final String[] events = {"Kino", "Konzert", "Essen gehen", "Spazieren gehen", "Fernsehen", "Rheinschwimmen", "Bleiben Sie Zuhause (Alain Berset, 2020)"};
-		final ObservableList<String> entries = FXCollections.observableArrayList (events);
-		final ListView<String> listView = new ListView<>(entries);
 		
+
 		Button addButton = new Button("Neu");
 		Button button = new Button("Klick mich");
 		TextField textField = new TextField();
-		
+
 		HBox hbox = new HBox(addButton, textField, button);
 		addButton.setOnAction(evt -> {entries.add(textField.getText());});
-		
-		button.setOnAction(evt -> button.setText(zufaelligeAntwort(events)));
-		
+
+		button.setOnAction(evt -> button.setText(zufaelligeAntwort(auswahl)));
+
 		VBox vBox = new VBox(hbox, listView);
-		
+
 		Scene scene = new Scene(vBox, 500, 300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
