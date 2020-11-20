790ce2e55a9be496b11cc339b6f01c1abf50f25b
Mischa Kissling <mischa@kissling.ch>
2020-11-20 13:02:48 +0100

    Decision Button 2 is running

--- src/main/java/ch/trinat/edu/javafx/decisionbutton2/DecisionButton2.java
+++ src/main/java/ch/trinat/edu/javafx/decisionbutton2/DecisionButton2.java
@@ -5,6 +5,8 @@ import java.util.Vector;
 
 import javafx.application.Application;
 import javafx.collections.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -12,39 +14,87 @@ import javafx.stage.Stage;
 
 public class DecisionButton2 extends Application {
 
-	public String zufaelligeAntwort(Vector <String> options) {
-
-		int random = (int)(Math.random()*options.size());
-		return options.get(random);
-	}
-
+	//Liste mit Aktivitäten erstellen
+	String[]activityList = {};
+	ObservableList <String> entries = FXCollections.observableArrayList(activityList);
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) {
+		try {
 
-		primaryStage.setTitle("Entscheidungsknopf");
-		String[] events = {"Kino", "Konzert", "Essen gehen", "Spazieren gehen", "Fernsehen", "Rheinschwimmen", "Bleiben Sie Zuhause (Alain Berset, 2020)"};
-		Vector<String> auswahl = new Vector<>(Arrays.asList(events));
+			//TextFeld für Eingabe von Aktivitäten
+			TextField textField = new TextField();
 
-		ObservableList<String> entries = FXCollections.observableArrayList (auswahl);
-		ListView<String> listView = new ListView<>(entries);
+			//Label zu TextField
+			Label labelforText = new Label("Aktion: ");	
 
+			//Speichern Button
+			Button saveButton = new Button("Speichern");
 
+			//HBox erstellen
+			HBox hBox = new HBox();
+			hBox.getChildren().addAll(labelforText, textField, saveButton);
+			hBox.setSpacing(25);
 
-		Button addButton = new Button("Neu");
-		Button button = new Button("Klick mich");
-		TextField textField = new TextField();
+			//TextFeld
+			saveButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					//Falls leer
+					if (textField.getText().equals("")) {
+						System.out.println("Leerer Eintrag nicht erlaubt");
+					}
+					else if (entries.contains(textField.getText())) {
+						//Falls in Liste bereits vorhanden
+						textField.clear();
+						System.out.println("Haben Sie bereits eingetragen");
+					}
+					else {
+						entries.add(textField.getText());
+					}
+					//Nach Eintrag TextField leeren
+					textField .clear();
 
-		HBox hbox = new HBox(addButton, textField, button);
-		addButton.setOnAction(evt -> {entries.add(textField.getText());});
+				}
+			});
 
-		button.setOnAction(evt -> button.setText(zufaelligeAntwort(auswahl)));
+			//Button der Aktionen ausgibt
+			Button button = new Button("Klick mich");
+			button.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					if (entries.size() == 0) {
+						button.setText("Aktion eingeben");
+					}
+					else {
+						button.setText(selectActivity());
+					}
+					primaryStage.setTitle("Entscheidungsknopf 2.0");
+					primaryStage.show();
+				}
+			});
+			
+			BorderPane layout = new BorderPane();
+			layout.setCenter(button);
+			layout.setTop(hBox);
+			
+			Scene scene = new Scene(layout, 500, 500);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-		VBox vBox = new VBox(hbox, listView);
-
-		Scene scene = new Scene(vBox, 500, 300);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	public String selectActivity() {
+		int position = (int) (Math.random() * entries.size());
+		if (position == entries.size()) {
+			position = position - 1;
+		}
+		String text = entries.get(position);
+		return text;
 	}
 
 	public static void main(String[] args) {
