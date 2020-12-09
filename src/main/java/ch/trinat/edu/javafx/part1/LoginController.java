9b1e7b987b0d2beedbc312e1caf89bb381942800
Mehinovic Haris <haris.mehinovic2001@gmail.com>
2020-12-09 16:52:26 +0100

    logincontroller

--- src/main/java/ch/trinat/edu/javafx/part1/LoginController.java
+++ src/main/java/ch/trinat/edu/javafx/part1/LoginController.java
@@ -3,19 +3,23 @@ package ch.trinat.edu.javafx.part1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController {
+int	counter = 0;
  @FXML
  private PasswordField passwordField;
  @FXML
  private TextField nameField;
  @FXML
+ Label lab;
+ @FXML
  protected void handleSubmitButtonAction(ActionEvent event)
  {
  System.out.println("Signin button user input: "
  + nameField.getText() + " pwd: "
- + passwordField.getText());
+ + passwordField.getText() );
  }
 }
